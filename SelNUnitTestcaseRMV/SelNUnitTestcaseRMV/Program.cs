06c169bc4d6ca4e5ecaf817167a2ace22475a89c
nagappank <krishnan.nagappan@gmail.com>
2014-07-22 14:12:49 +0200

    First Commt Message

---
+++ SelNUnitTestcaseRMV/SelNUnitTestcaseRMV/Program.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace SelNUnitTestcaseRMV
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Navigate().GoToUrl("http://www.rmv.de");
+            //driver.Navigate().GoToUrl(baseURL + "/");
+            driver.FindElement(By.Id("myname")).Clear();
+            driver.FindElement(By.Id("myname")).SendKeys("krishnan.nagappan@gmail.com");
+            driver.FindElement(By.Id("mypw")).Clear();
+            driver.FindElement(By.Id("mypw")).SendKeys("Keith)98");
+            //driver.FindElement(By.CssSelector("button.a11y-focus")).Click();
+            driver.FindElement(By.XPath("//*[text()=\"Login\"]")).Click();
+            driver.FindElement(By.Id("searchbar")).Click();
+            driver.FindElement(By.Id("searchbar")).Clear();
+            driver.FindElement(By.Id("searchbar")).SendKeys("hamburg");
+            driver.FindElement(By.XPath("//*[text()=\"Suchen\"]")).Click();
+            driver.FindElement(By.LinkText("Logout")).Click();
+
+            driver.Close();
+        }
+    }
+}
